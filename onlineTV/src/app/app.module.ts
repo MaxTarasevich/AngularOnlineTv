97f02b461736bd885bdeaf977a2340b9a8a92097
Max <maxtarasevich@gmail.com>
2021-04-12 16:28:06 +0300

    basic components

--- onlineTV/src/app/app.module.ts
+++ onlineTV/src/app/app.module.ts
@@ -4,11 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ButtonComponent } from './button/button.component';
+import { TrendingTNShowComponent } from './trending-tnshow/trending-tnshow.component';
+import { TrendingMoviesComponent } from './trending-movies/trending-movies.component';
+import { GenresComponent } from './genres/genres.component';
+import { ActorsComponent } from './actors/actors.component';
+import { CommingSoonComponent } from './comming-soon/comming-soon.component';
+import { RegisterNowComponent } from './register-now/register-now.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BannerComponent,
+    ButtonComponent,
+    TrendingTNShowComponent,
+    TrendingMoviesComponent,
+    GenresComponent,
+    ActorsComponent,
+    CommingSoonComponent,
+    RegisterNowComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ onlineTV/src/app/trending-tnshow/trending-tnshow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trending-tnshow',
+  templateUrl: './trending-tnshow.component.html',
+  styleUrls: ['./trending-tnshow.component.scss']
+})
+export class TrendingTNShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
